46fa742cd97e4b5e8c533858d2dd3b90d27e1224
revyll ogandaga <revyll44@gmail.com>
2024-11-25 18:02:56 +0300

    com

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_25_140402) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "passports", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "description"
+    t.decimal "price", precision: 8, scale: 2
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
