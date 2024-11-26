fef7ebb6f2036764f12f89306ca2448cf819a30b
michael uche <bonjourfamily28@gmail.com>
2024-11-26 15:47:52 +0200

    added some views

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,28 +1,51 @@
 class BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = @passport.bookings.find(params[:id])
+  end
+
   def new
-    @passport = Passport.find(params[:passport_id])
-    @booking = @passport.bookings.build
+    @booking = @passport.bookings.new
   end
 
   def create
-    @passport = Passport.find(params[:passport_id])
-    @booking = @passport.bookings.build(booking_params.merge(user: current_user))
-    if @booking.save
-      redirect_to @passport, notice: 'Booking created successfully.'
-    else
-      render :new, status: :unprocessable_entity
-    end
+   @booking = @passport.bookings.new(booking_params)
+   @booking.user = current_user
+   if @booking.save
+    redirect_to passport_booking_path(@passport,@booking), notice: 'booking successfully created'
+   else
+    render :new, status: :unprocessable_entity
+   end
+  end
+
+  def edit
+    @booking = @flat.bookings.find(params_id)
+
+  end
+
+  def update
+    @passport = passport.find(params[:passport_id])
+    @booking = @flat.bookings.find(params_id)
+      if @booking.update(booking_params)
+        redirect_to passport_booking_path(@passport,@booking), notice: 'booking successfully updated'
+       else
+        render :edit, status: :unprocessable_entity
+      end
   end
 
   def destroy
     @booking = Booking.find(params[:id])
     @booking.destroy
-    redirect_back fallback_location: passport_path, notice: 'Booking deleted successfully.'
+    redirect_to passport_bookings_path(@passport), notice: 'Booking deleted successfully.'
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:status)
+    params.require(:booking).permit(:status, :start_date, :end_date)
   end
 end
