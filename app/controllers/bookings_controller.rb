a76c311e67d72b57b4034ba4db2dbeadcf80e90c
revyll ogandaga <revyll44@gmail.com>
2024-11-26 13:23:29 +0300

    pulling

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,28 @@
+class BookingsController < ApplicationController
+  def new
+    @passport = Passport.find(params[:passport_id])
+    @booking = @passport.bookings.build
+  end
+
+  def create
+    @passport = Passport.find(params[:passport_id])
+    @booking = @passport.bookings.build(booking_params.merge(user: current_user))
+    if @booking.save
+      redirect_to @passport, notice: 'Booking created successfully.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_back fallback_location: passport_path, notice: 'Booking deleted successfully.'
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:status)
+  end
+end
