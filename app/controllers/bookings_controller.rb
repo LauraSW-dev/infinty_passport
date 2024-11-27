9e659e7065409d4f259550ba3a4c7b03a5ea32eb
Laura Mngwengwe <systemsengineer707@gmail.com>
2024-11-27 17:46:47 +0200

    approve and reject done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -19,21 +19,21 @@ class BookingsController < ApplicationController
     @booking = Booking.new
   end
 
-    def create
-      @passport = Passport.find(params[:passport_id])
-      @booking = @passport.bookings.new(booking_params)
-      @booking.user = current_user
+  def create
+    @passport = Passport.find(params[:passport_id])
+    @booking = @passport.bookings.new(booking_params)
+    @booking.user = current_user
 
-      if @booking.save
-        redirect_to bookings_path, notice: 'Booking successfully created'
-      else
-        render :new, status: :unprocessable_entity
-      end
+    if @booking.save
+      redirect_to passport_bookings_path(@passport, @booking), notice: 'Booking successfully created'
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def edit
-    @booking = @passport.bookings.find(params[:id])
-    end
+  def edit
+  @booking = @passport.bookings.find(params[:id])
+  end
 
   def update
     @passport = passport.find(params[:passport_id])
@@ -52,8 +52,9 @@ class BookingsController < ApplicationController
   end
 
   def approve
-    @booking = @passport.bookings.find(param[:id])
-    @booking.approve!
+    @passport = Passport.find_by(id: params[:passport_id])
+    @booking = @passport.bookings.find(params[:id])
+    @booking.approved!
     respond_to do |format|
       format.html { redirect_to passport_path(@booking.passport), notice: "Booking approved!"}
       format.json
@@ -61,7 +62,8 @@ class BookingsController < ApplicationController
   end
 
   def reject
-    @booking = @passport.bookings.find(param[:id])
+    @passport = Passport.find_by(id: params[:passport_id])
+    @booking = @passport.bookings.find(params[:id])
     @booking.rejected!
     respond_to do |format|
       format.html {redirect_to passport_path(@booking.passport), notice: "Booking has been rejected!"}
