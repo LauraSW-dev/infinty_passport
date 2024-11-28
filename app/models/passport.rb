31b1278746207eb28a658f05d5fa189f376de792
michael uche <bonjourfamily28@gmail.com>
2024-11-28 14:27:45 +0200

    seed fix

--- app/models/passport.rb
+++ app/models/passport.rb
@@ -1,6 +1,6 @@
 class Passport < ApplicationRecord
   has_many :bookings
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   has_many_attached :photos
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
